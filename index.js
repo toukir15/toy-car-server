4cd472dd86f9187c00c304625601bd6d10c203b5
Tanvir <toukir.developer.bd@gmail.com>
2023-05-20 01:05:32 +0600

    Get Oparation: get data by specific id

--- index.js
+++ index.js
@@ -41,8 +41,16 @@ async function run() {
             if (req?.query?.category) {
                 query = { category: req.query.category }
             }
-            console.log(query);
             const result = await carsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await carsCollection.findOne(query)
             res.send(result)
         })
 
